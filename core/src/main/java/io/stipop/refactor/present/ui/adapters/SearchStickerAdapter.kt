36baf0875fc00c3743a18fb2031696dc55885189
ainus <dev@ainus.co.kr>
2021-09-11 18:47:25 +0900

    Fix - AS-5

--- core/src/main/java/io/stipop/refactor/present/ui/adapters/SearchStickerAdapter.kt
+++ core/src/main/java/io/stipop/refactor/present/ui/adapters/SearchStickerAdapter.kt
@@ -1,18 +1,11 @@
 package io.stipop.refactor.present.ui.adapters
 
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
-import com.bumptech.glide.request.FutureTarget
-import com.bumptech.glide.request.target.ViewTarget
 import io.stipop.databinding.ItemStickerBinding
 import io.stipop.refactor.domain.entities.SPStickerItem
-
 
 class SearchStickerAdapter :
     ViewBindingAdapter<SPStickerItem, ItemStickerBinding>() {
@@ -20,14 +13,13 @@ class SearchStickerAdapter :
     class SearchStickerViewHolder(binding: ItemStickerBinding) :
         ViewBindingHolder<SPStickerItem, ItemStickerBinding>(binding) {
 
-
-
         override fun onBind(item: SPStickerItem) {
-            Glide.with(binding.stickerImage)
+
+            Glide.with(itemView)
                 .load(item.stickerImg)
                 .transition(withCrossFade())
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(binding.stickerImage)
+                .clearOnDetach()
         }
     }
 
