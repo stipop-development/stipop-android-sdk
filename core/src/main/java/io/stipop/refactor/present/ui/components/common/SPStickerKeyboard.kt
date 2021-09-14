344012495477970a94d716e2f6569da2f0753b94
ainus <dev@ainus.co.kr>
2021-09-14 10:47:59 +0900

    Fix - AS-5

--- core/src/main/java/io/stipop/refactor/present/ui/components/common/SPStickerKeyboard.kt
+++ core/src/main/java/io/stipop/refactor/present/ui/components/common/SPStickerKeyboard.kt
@@ -11,7 +11,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import android.widget.FrameLayout
 import android.widget.PopupWindow
